4386ce6460b2423d1510be60ea1302e6611e6d67
wadee3 <wadeealmahmoud369@gmail.com>
2026-06-27 12:32:45 +0300

    fix:resolve cart item name layout issue

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -108,7 +108,7 @@ useEffect(()=>{
                     className="w-20 h-20 object-contain rounded-xl"
                   />
                   <div className="flex-1 min-w-0">
-                    <h3 className="text-lg font-semibold">{items.name}</h3>
+                    <h3 className="text-lg font-semibold truncate">{items.name}</h3>
                     <p className="text-sm text-gray-300">
                       {" "}
                       الكمية: {items.quantity}
